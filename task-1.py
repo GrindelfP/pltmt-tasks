5aa45c3d097b5edf30addd0e0ff928d539dbf1ed
GrindelfP <grindelf.perlomutrovij@gmail.com>
2024-12-20 20:19:50 +0300

    final complete task-1

--- task-1.py
+++ task-1.py
@@ -1,7 +1,7 @@
 import re
 
 
-def process_code(string: str) -> tuple[str, set]:
+def process_code(string: str) -> tuple[str, str, set]:
 
     without_comments: str = re.sub(r'/\*.*?\*/', '', string, flags=re.DOTALL)
     without_extra_spaces: str = re.sub(r'\s+', ' ', without_comments).strip()
@@ -12,14 +12,17 @@ def process_code(string: str) -> tuple[str, set]:
 
     lexemes: set = set(re.findall(r'[a-zA-Zа-яА-ЯёЁ0-9]+|[^\s\w]', cleaned_code))
 
-    return cleaned_code, lexemes
+    return without_comments, cleaned_code, lexemes
 
 
 if __name__ == "__main__":
     input_str = input("Input you code: ")
-    correct_code, lexemes_set = process_code(input_str)
+    no_comments_code, correct_code, lexemes_set = process_code(input_str)
 
-    print("processed code:", correct_code)
+    print("Processed code without comments:", no_comments_code)
+    print("Processed code without spaces:", correct_code)
     print("Lexemes:")
     for lexeme in lexemes_set:
         print(lexeme)
+
+    input("Press ENTER key to exit...")
